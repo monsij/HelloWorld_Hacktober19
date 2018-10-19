bbc774b15d661817fe86e82141739b605677c86e
Viet Than <thanhoangviet@gmail.com>
2018-10-18 20:33:12 -0500

    Create HelloWorld_Cplusplus_QT.cpp

---
+++ HelloWorld_Cplusplus_QT.cpp
@@ -0,0 +1,18 @@
+// Hello World in C++ for the Qt framework
+// Author: Viet Than
+// Contact: thanhoangviet@gmail.com, viet.than@vanderbilt.edu
+
+#include <qapplication.h>
+#include <qlabel.h>
+
+int main(int argc, char *argv[])
+{
+  QApplication a(argc, argv);
+  QLabel l("Hello World!", 0);
+  l.setCaption("Test");
+  l.setAlignment(Qt::AlignCenter);
+  l.resize(300, 200);
+  a.setMainWidget(&l);
+  l.show();
+  return(a.exec());
+}
