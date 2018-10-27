26fdbc276172071ab9af351c3908444705750c61
Ravi Kant Pandey <ravikantvicky@gmail.com>
2018-10-27 15:37:27 +0530

    created hello-world.php

---
+++ hello-world.php
@@ -0,0 +1,4 @@
+<?php
+echo '<h1 style="text-align:center;color: #009688;">Hello World !</h1>';
+echo '<h1 style="text-align:center;color: #FF5722;">Welcome to <span style="font-size: 42;color: cadetblue;">Hacktober18 !</span></h1>';
+?>
